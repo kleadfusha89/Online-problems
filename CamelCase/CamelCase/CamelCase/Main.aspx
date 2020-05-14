aca766f3c5688e8f43b922549f266609c65658e6
Klead Fusha <kleadfusha@lewisu.edu>
2020-05-13 23:31:32 -0500

    Adding a CamelCase and gOOGLEcASE project

---
+++ CamelCase/CamelCase/CamelCase/Main.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Main.aspx.cs" Inherits="CamelCase.Main" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+
+        </div>
+        
+        <p>
+            <asp:RequiredFieldValidator ID="reqtxtEnterSomething" runat="server" ErrorMessage="Please Enter Something" Visible="false" ControlToValidate="txtEnterSomething" OnDataBinding="btnCamelCase_Click"></asp:RequiredFieldValidator>
+            <asp:TextBox ID="txtEnterSomething" runat="server" Height="300px" Width="300px" BackColor="#E5E5E5" BorderColor="#37B3C8" BorderStyle="Inset" BorderWidth="3px" Columns="40" TextMode="MultiLine"></asp:TextBox><br />
+            <asp:Button ID="btnCamelCase" runat="server" Text="CamelCase" OnClick="btnCamelCase_Click" Width="308.4px" BackColor="#E5E5E5" BorderColor="#37B3C8" BorderStyle="Inset" BorderWidth="3px" /><br />
+            <asp:Button ID="btnGoogleCase" runat="server" Text="gOOGLEcASE" OnClick="btnGoogleCase_Click" Width="308.4px" BackColor="#E5E5E5" BorderColor="#37B3C8" BorderStyle="Inset" BorderWidth="3px" />
+            
+            <br /><br />
+            <asp:TextBox ID="txtShow" ReadOnly="true" runat="server" Visible="false" Height="300px" Width="300px" BackColor="#E5E5E5" BorderColor="#37B3C8" BorderStyle="Inset" BorderWidth="3px" TextMode="MultiLine" ></asp:TextBox>
+            
+           
+            <asp:Button ID="btnClearCamelgOOGLE" runat="server" Text="cLEAR" OnClick="btnClear_Click" Width="308.4px" BackColor="#E5E5E5" BorderColor="#37B3C8" BorderStyle="Inset" BorderWidth="3px" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ CamelCase/CamelCase/CamelCase/Main.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CamelCase
+{
+    public partial class Main : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        protected void btnCamelCase_Click(object sender, EventArgs e)
+        {
+            string txtInput = txtEnterSomething.Text;
+            string CamelCase = "";
+
+            
+            if (txtInput.Length > 0 && txtInput[0] != ' ')
+               CamelCase += Char.ToUpper(txtInput[0]);
+            
+            for (int i = 1; i < txtInput.Length; i++)
+            {                               
+
+                if (txtInput[i] != ' ' && txtInput[i - 1] == ' ')
+                    CamelCase += Char.ToUpper(txtInput[i]);
+
+                else if (txtInput[i] != ' ' && txtInput[i - 1] != ' ')
+                    CamelCase += Char.ToLower(txtInput[i]);
+            }
+            txtShow.Visible = true;
+            txtShow.Text = CamelCase;
+
+
+        }
+
+        protected void btnGoogleCase_Click(object sender, EventArgs e)
+        {
+
+            string txtInput = txtEnterSomething.Text;
+            string googleCase = "";
+
+            if (txtInput.Length > 0 && txtInput[0] != ' ')
+                googleCase += Char.ToUpper(txtInput[0]);
+            for (int i = 1; i < txtInput.Length; i++)
+            {
+
+                if (txtInput[i] != ' ' && txtInput[i - 1] == ' ')
+                    googleCase += Char.ToLower(txtInput[i]);
+
+                else if (txtInput[i] != ' ' && txtInput[i - 1] != ' ')
+                    googleCase += Char.ToUpper(txtInput[i]);
+            }
+            txtShow.Visible = true;
+            txtShow.Text = googleCase;
+        }
+
+       
+
+    
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            txtEnterSomething.Text = "";
+            txtShow.Text = "";
+            txtShow.Visible = false;
+        }
+    }
+}
